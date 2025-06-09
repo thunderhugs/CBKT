3fe6d4f9aa41cc1053b73cfad5726ee7dbb49bed
thunderhugs <dan.jd.green+github@gmail.com>
2025-06-09 19:36:13 +0100

    Add files via upload

--- grand_prix_graphic.py
+++ grand_prix_graphic.py
@@ -286,20 +286,20 @@ if __name__ == "__main__":
     alpha_players = ["Jackson", "Green", "Day", "Worgan", "Allcut", "Crockwell", "Jasper"]
     alpha_league = KillTeamLeague(alpha_players, "Alpha")
     # Add example match
-    alpha_league.add_result("Allcut", "Green", "L")
-    alpha_league.add_result("Green", "Allcut", "W")
+    alpha_league.add_result("Allcut", "Green", "W")
+    alpha_league.add_result("Green", "Allcut", "L")
     alpha_league.add_result("Day", "Crockwell", "W")
     alpha_league.add_result("Crockwell", "Day", "L")
-    alpha_league.add_result("Green", "Day", "W")
-    alpha_league.add_result("Day", "Green", "L")
+    alpha_league.add_result("Green", "Day", "L")
+    alpha_league.add_result("Day", "Green", "W")
     alpha_league.add_result("Worgan", "Allcut", "W")
     alpha_league.add_result("Allcut", "Worgan", "L")
     alpha_league.add_result("Crockwell", "Jasper", "W")
     alpha_league.add_result("Jasper", "Crockwell", "L")
-    alpha_league.add_result("Worgan", "Green", "L")
-    alpha_league.add_result("Green", "Worgan", "W")
-    alpha_league.add_result("Jackson", "Green", "L")
-    alpha_league.add_result("Green", "Jackson", "W")
+    alpha_league.add_result("Worgan", "Green", "W")
+    alpha_league.add_result("Green", "Worgan", "L")
+    alpha_league.add_result("Jackson", "Green", "W")
+    alpha_league.add_result("Green", "Jackson", "L")
     alpha_league.add_result("Jackson", "Allcut", "W")
     alpha_league.add_result("Allcut", "Jackson", "L")
     alpha_league.add_result("Worgan", "Day", "W")
@@ -322,7 +322,7 @@ if __name__ == "__main__":
     alpha_league.victory_points[2] = 33  + 13 + 17 # Day's VP
     alpha_league.victory_points[5] = 28  + 8 + 5 + 11# Crockwell's VP
     alpha_league.victory_points[4] = 29  + 2 + 20 + 17 + 21 # Allcuts's VP
-    alpha_league.victory_points[1] = 24 + 13 + 19 + 19 + 50# Green's VP
+    alpha_league.victory_points[1] = 24 + 13 + 19 + 19 # Green's VP
     alpha_league.victory_points[3] = 35 + 14 + 17   # Worgan's VP
     alpha_league.victory_points[6] = 13  + 4 + 5# Jasper's VP
     
@@ -362,8 +362,6 @@ if __name__ == "__main__":
     bravo_league.add_result("Parrot", "Quy", "L")
     bravo_league.add_result("Hodgson", "Kompart", "L")
     bravo_league.add_result("Kompart", "Hodgson", "W")
-    bravo_league.add_result("Hodgson", "Charlie", "L")
-    bravo_league.add_result("Charlie", "Hodgson", "W")
 
 
     # Add some example victory points
@@ -371,7 +369,7 @@ if __name__ == "__main__":
     bravo_league.victory_points[1] = 45 + 16  # Ambrose's VP
     bravo_league.victory_points[5] = 4 + 7 + 14 + 11 + 15# Hodgson's VP
     bravo_league.victory_points[4] = 38 + 15 + 9 + 6 # Parrots's VP
-    bravo_league.victory_points[6] = 28  + 6 + 20 # Charlie's VP
+    bravo_league.victory_points[6] = 28  + 6 # Charlie's VP
     bravo_league.victory_points[3] = 27 + 19 + 20 + 20 #Dun's
     bravo_league.victory_points[2] = 23  + 14 + 17 + 19 #Max
     
